85c5fb84e3d2b4c651b384e28416bac0da72b5f6
sergii.korobko <sergii.korobko@accenture.com>
2017-10-26 01:39:49 +0300

    Created database script, configured hibernate, added SessionFactoryHelper class and entity for Roles table

---
+++ mysql/realestate_database.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE IF NOT EXISTS realestate;
+
+USE realestate;
+
+CREATE USER IF NOT EXISTS 'admin'@'realestate' IDENTIFIED BY 'admin';
+
+GRANT ALL PRIVILEGES ON * . * TO 'admin'@'realestate';
+
+SET foreign_key_checks = 0;
+
+DROP TABLE IF EXISTS `Roles`;
+CREATE TABLE `Roles` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `Roles` (`name`) VALUES ('ROLE_ADMIN');
+INSERT INTO `Roles` (`name`) VALUES ('ROLE_OWNER');
+INSERT INTO `Roles` (`name`) VALUES ('ROLE_BUYER');
+
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(128) NOT NULL,
+  `password` VARCHAR(128) NOT NULL,
+  `roleId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`roleId`) REFERENCES `Roles`(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `Users` (`login`, `password`, `roleId`) VALUES ('admin', 'admin', (SELECT id
+                                                            FROM `Roles`
+                                                            WHERE `name` = 'ROLE_ADMIN'));
+
+INSERT INTO `Users` (`login`, `password`, `roleId`) VALUES ('owner', 'owner', (SELECT id
+                                                            FROM `Roles`
+                                                            WHERE `name` = 'ROLE_OWNER'));
+
+INSERT INTO `Users` (`login`, `password`, `roleId`) VALUES ('buyer', 'buyer', (SELECT id
+                                                            FROM `Roles`
+                                                            WHERE `name` = 'ROLE_BUYER'));
+
+DROP TABLE IF EXISTS `RealEstateStatus`;
+CREATE TABLE `RealEstateStatus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `RealEstates`;
+CREATE TABLE `RealEstates` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(512) NOT NULL,
+  `description` VARCHAR(1024) NOT NULL,
+  `ownerId` INT(11) NOT NULL,
+  `statusId` INT(11) NOT NULL,
+  `adminNote` VARCHAR(512) NOT NULL,
+  `address` VARCHAR(512) NOT NULL,
+  `price` decimal(6,2) NOT NULL,
+  `nearbyLocations` VARCHAR(512) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`ownerId`) REFERENCES `Users`(id) ON UPDATE CASCADE,
+  FOREIGN KEY (`statusId`) REFERENCES `RealEstateStatus`(id) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `Messages`;
+CREATE TABLE `Messages` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `senderId` INT(11) NOT NULL,
+  `recipientId` INT(11) NOT NULL,
+  `message` VARCHAR(512) NOT NULL,
+  `statusId` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`recipientId`) REFERENCES `Users`(id),
+  FOREIGN KEY (`senderId`) REFERENCES `Users`(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET foreign_key_checks = 1;
+
+
---
+++ src/main/java/com/qssoft/entities/Role.java
@@ -0,0 +1,40 @@
+package com.qssoft.entities;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "Roles" )
+public class Role
+{
+    private int id;
+    private String name;
+
+    public Role() {
+    }
+
+    @Id
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
